5fd372214def2eed796f16bfdbca69b66f2a7bf5
deepak <dipakjamdar@gmail.com>
2021-07-21 21:03:48 +0530

    changes in main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  const [counter, setCounter] = useState(0);
+ 
+  // handle button click event
+  const handleClick = () => {
+    setCounter(counter + 1);
+  }
+ 
+  // handle button click event to add 5 in counter
+  const handleAddFiveClick = () => {
+    for (let i = 0; i < 5; i++) {
+      setCounter(counter + 1);
+    }
+  }
+
+  const handleAddFivePrvClick = () => {
+    for (let i = 0; i < 5; i++) {
+      setCounter(prevState => prevState + 1);
+    }
+  }
+ 
+  return <div className="App">
+    <label>Counter: {counter}</label><br /><br />
+    <button onClick={handleClick}>Add +1</button> 
+    <button onClick={handleAddFiveClick}>Add +5</button>
+    <button onClick={handleAddFivePrvClick}>Add +5(previous state)</button>
+  </div>
+  
+  
 }
 
 export default App;
